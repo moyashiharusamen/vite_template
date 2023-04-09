9862b365f234c276048cf1cd88f90283f87c0975
moyashiharusamen <moyashi.harusamen@gmail.com>
2023-04-09 18:15:18 +0900

    feat: stylelint を導入

---
+++ .stylelintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    "stylelint-config-standard-scss",
+    "stylelint-config-prettier-scss"
+  ],
+  ignoreFiles: [],
+  rules: {
+    "scss/at-mixin-argumentless-call-parentheses": "always",
+    "selector-class-pattern": null,
+    "function-name-case": null,
+    "alpha-value-notation": null,
+  }
+};
