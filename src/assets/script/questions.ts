212abec039c63b3ab29c5536eb7b025f2606da7e
moyashiharusamen <moyashi.harusamen@gmail.com>
2023-04-10 14:55:31 +0900

    feat: チェック機能を作成

---
+++ src/assets/script/questions.ts
@@ -0,0 +1,28 @@
+/**
+ * @type {Array<Object>} 設問群
+ */
+const questions = [
+  {
+    /**
+     * @type {number} 設問の ID
+     */
+    id: 1,
+
+    /**
+     * @type {string} 設問
+     */
+    title: 'タイトル1',
+
+    /**
+     * @type {Array<String>} 選択肢
+     */
+    choice: ['選択肢1', '選択肢2']
+  },
+  {
+    id: 2,
+    title: 'タイトル2',
+    choice: ['選択肢1', '選択肢2']
+  }
+]
+
+export default questions
