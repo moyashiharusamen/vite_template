3c268676286fc8a9e12428f34515b8c3a01425cc
moyashiharusamen <moyashi.harusamen@gmail.com>
2023-04-12 21:22:02 +0900

    feat: チェックの別パターンを作成

---
+++ src/assets/script/Check2.vue
@@ -0,0 +1,245 @@
+<template>
+  <div class="root">
+    <transition name="slide">
+      <div class="body">
+        <div
+          class="question"
+          v-if="!isResultDisplay"
+        >
+          <div
+            class="num"
+            :key="currentQuestionId"
+          >
+            <p>
+              Q<span>{{ currentQuestion.id }}</span>
+            </p>
+          </div>
+
+          <div class="title">
+            <p>{{ currentQuestion.title }}</p>
+          </div>
+
+          <div
+            class="choice"
+            v-for="(item, i) in currentQuestion.choice"
+            :key="item"
+          >
+            <button
+              type="button"
+              aria-checked="false"
+              role="radio"
+              :data-button-id="i"
+              @click="handleClickChoiceButton($event)"
+            >
+              {{ item }}
+            </button>
+          </div>
+
+          <div class="controller">
+            <button
+              type="button"
+              class="back"
+              :disabled="disabledBackButton"
+              :aria-disabled="disabledBackButton"
+              @click="handleClickBackButton"
+            >
+              戻る
+            </button>
+
+            <button
+              type="button"
+              class="next"
+              :disabled="disabledNextButton"
+              :aria-disabled="disabledNextButton"
+              @click="handleClickNextButton"
+            >
+              次へ
+            </button>
+
+            <span class="progress">{{ currentQuestionId + 1 }}/{{ q.length }}</span>
+          </div>
+
+          <div
+            class="toResult"
+            v-if="isAllAnswered && isLastQuestion()"
+          >
+            <button
+              type="button"
+              @click="isResultDisplay = !isResultDisplay"
+            >
+              結果を表示する
+            </button>
+          </div>
+        </div>
+
+        <div
+          class="answer"
+          v-else
+        >
+          <div
+            v-for="item in answers"
+            :key="item"
+          >
+            <p><span>id</span>{{ item.id }}</p>
+            <p><span>title</span>{{ item.title }}</p>
+            <p><span>answer</span>{{ item.answer }}</p>
+          </div>
+
+          <div class="return">
+            <button
+              type="button"
+              @click="returnCheck"
+            >
+              もう一度チェックする
+            </button>
+          </div>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, reactive, ref } from 'vue'
+import questions from './questions'
+
+// -------------------- Data --------------------
+/**
+ * @type {Object[]} 設問群
+ */
+const q = reactive(questions)
+/**
+ * @type {boolean} 結果を表示するなら true
+ */
+const isResultDisplay = ref(false)
+/**
+ * @type {boolean} 全ての設問に回答していれば true
+ */
+const isAllAnswered = ref(false)
+/**
+ * @type {number} 現在の表示する設問の数
+ */
+const currentQuestionId = ref(0)
+/**
+ * @type {Object[]} 回答群
+ */
+let answers: any = reactive([])
+/**
+ * @type {number} 設問の最後の ID
+ */
+const lastQustionId = q.length - 1
+/**
+ * @type {boolean} 前へボタンを非活性にしたいときに true にする
+ */
+const disabledBackButton = ref(true)
+/**
+ * @type {boolean} 次へボタンを非活性にしたいときに true にする
+ */
+const disabledNextButton = ref(true)
+/**
+ * @type {Object} 現在表示されるべき設問
+ */
+const currentQuestion = computed(() => q[currentQuestionId.value])
+/**
+ * currentQuestionID が最初の設問を示している場合 true が返る
+ * @return {boolean}
+ */
+const isFirstQuestion = () => currentQuestionId.value === 0
+/**
+ * currentQuestionID が最後の設問を示している場合 true が返る
+ * @return {boolean}
+ */
+const isLastQuestion = () => lastQustionId === currentQuestionId.value
+
+// -------------------- Methods --------------------
+/**
+ * currentQuestionID の値をインクリメントする
+ * @return {Void}
+ */
+const incrementCurrentQuestionId = () => {
+  if (isLastQuestion()) return
+  currentQuestionId.value++
+}
+/**
+ * currentQuestionID の値をデクリメントする
+ * @return {Void}
+ */
+const decrementCurrentQuestionId = () => {
+  if (isFirstQuestion()) return
+  currentQuestionId.value--
+}
+/**
+ * 前へ次へボタンを活性にするかどうかのチェックを行う
+ * 最初の設問を表示しているとき、または表示されているより前の設問に回答済みの場合は、前へボタンを非活性にする
+ * 最後の設問を表示しているとき、または表示されているより後の設問に回答済みの場合は、次へボタンを非活性にする
+ * @return {Void}
+ */
+const checkPaginationButton = () => {
+  disabledBackButton.value = isFirstQuestion() || !answers[currentQuestionId.value - 1]
+  disabledNextButton.value = isLastQuestion() || !answers[currentQuestionId.value + 1]
+}
+/**
+ * 回答をデータとして保存する
+ * @param {Object} e $event
+ * @return {Void}
+ */
+const saveAnswer = (e: any) => {
+  const id = currentQuestionId.value + 1
+  const dataButtonId = e.target.dataset.buttonId
+  const title = currentQuestion.value.title
+  const answer = currentQuestion.value.choice[dataButtonId]
+  const content = { id: id, title: title, answer: answer }
+
+  answers[currentQuestionId.value] = content
+}
+/**
+ * チェックに戻る
+ * @return {Void}
+ */
+const returnCheck = () => {
+  answers = []
+  currentQuestionId.value = 0
+  isResultDisplay.value = false
+  isAllAnswered.value = false
+  checkPaginationButton()
+}
+
+// -------------------- イベント時に実行されるメソッド --------------------
+/**
+ * 選択肢ボタンを押したときに実行する
+ * @param {Object} e $event
+ * @return {Void}
+ */
+const handleClickChoiceButton = (e: any) => {
+  saveAnswer(e)
+  incrementCurrentQuestionId()
+  checkPaginationButton()
+
+  if (q.length === answers.length) isAllAnswered.value = true
+}
+/**
+ * 前へボタンを押したとき
+ * @return {Void}
+ */
+const handleClickBackButton = () => {
+  decrementCurrentQuestionId()
+  checkPaginationButton()
+}
+/**
+ * 次へボタンを押したとき
+ * @return {Void}
+ */
+const handleClickNextButton = () => {
+  incrementCurrentQuestionId()
+  checkPaginationButton()
+}
+</script>
+
+<style lang="scss" scoped>
+.controller > button {
+  transition: 0.2s opacity ease;
+}
+.controller > button[aria-disabled='true'] {
+  opacity: 0;
+}
+</style>
---
+++ src/assets/script/check2.ts
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import Check from './Check2.vue'
+
+createApp(Check).mount('#check')
