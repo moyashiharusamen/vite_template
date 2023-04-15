aeb95ffb1c8f11277fcb2ea10448add10c699153
moyashiharusamen <moyashi.harusamen@gmail.com>
2023-04-15 21:33:21 +0900

    feat: meta description を設定

--- page.js
+++ page.js
@@ -7,12 +7,15 @@ const page = {
   "/index.html": {
     isHome: true,
     title: "Top Page",
+    description: "Top page description",
   },
   "/hoge/index.html": {
     title: "Hoge Page",
+    description: "Hoge page description",
   },
   "/check/index.html": {
     title: "Check Page",
+    description: "Check page description",
   },
 };
 
