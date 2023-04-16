1c470f4f7fcc2ee9e4239703a0900cd125037917
moyashiharusamen <moyashi.harusamen@gmail.com>
2023-04-16 14:10:16 +0900

    refactor: prettier を適用

--- page.js
+++ page.js
@@ -1,26 +1,26 @@
 const defaultValue = {
-  title: "default title",
-  description: "default descrpition",
+  title: 'default title',
+  description: 'default descrpition'
 }
 
 const page = {
-  "/index.html": {
+  '/index.html': {
     isHome: true,
-    title: "Top Page",
-    description: "Top page description",
+    title: 'Top Page',
+    description: 'Top page description'
   },
-  "/hoge/index.html": {
-    title: "Hoge Page",
-    description: "Hoge page description",
+  '/hoge/index.html': {
+    title: 'Hoge Page',
+    description: 'Hoge page description'
   },
-  "/check/index.html": {
-    title: "Check Page",
-    description: "Check page description",
+  '/check/index.html': {
+    title: 'Check Page',
+    description: 'Check page description'
   },
-  "/check/index2.html": {
-    title: "Check2 Page",
-    description: "Check2 page description",
-  },
-};
+  '/check/index2.html': {
+    title: 'Check2 Page',
+    description: 'Check2 page description'
+  }
+}
 
-export default page;
+export default page
